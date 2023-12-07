9a2dd6b6aabcdaa99f5aafd3dacca5e015413407
Nalani Daniels <135375665+nalDaniels@users.noreply.github.com>
2023-12-07 01:00:40 -0500

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 // import mongoose from "mongoose";
 const mongoose = require("mongoose");
 const Connection = async (username, password) => {
-  const URL = `mongodb://${username}:${password}@ac-yiuldeo-shard-00-00.8pambxl.mongodb.net:27017,ac-yiuldeo-shard-00-01.8pambxl.mongodb.net:27017,ac-yiuldeo-shard-00-02.8pambxl.mongodb.net:27017/quiz-network?ssl=true&replicaSet=atlas-xlqrrv-shard-0&authSource=admin&retryWrites=true&w=majority`;
+  const URL = `mongodb+srv://kurafinalpj:kurafinalpj@cluster0.mjwldbe.mongodb.net/?retryWrites=true&w=majority`;
 
   try {
     await mongoose.connect(URL, {
